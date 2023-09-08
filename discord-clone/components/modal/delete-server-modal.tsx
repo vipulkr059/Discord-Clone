87226e165e76d1d7321912f946adc577099a38e4
vipulkr059 <vipulkr059@gmail.com>
2023-09-08 20:24:40 +0530

    feat : Delete Server Added

--- discord-clone/components/modal/delete-server-modal.tsx
+++ discord-clone/components/modal/delete-server-modal.tsx
@@ -28,7 +28,7 @@ const DeleteServer = () => {
   const onClick = async () => {
     try {
       setisLoading(true);
-      await axios.patch(`/api/servers/${server?.id}/leave`);
+      await axios.delete(`/api/servers/${server?.id}`);
       onClose();
       router.refresh();
       router.push("/");
@@ -44,14 +44,14 @@ const DeleteServer = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Leave Server
+            Delete Server
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
-            Are you sure you want to leave{" "}
+            Are you sure you want to do this ? <br />
             <span className="font-semibold text-indigo-500">
               {server?.name}
-            </span>
-            ?
+            </span>{" "}
+            will be permanatly deleted
           </DialogDescription>
         </DialogHeader>
         <DialogFooter className="bg-gray-100 px-6 py-4">
