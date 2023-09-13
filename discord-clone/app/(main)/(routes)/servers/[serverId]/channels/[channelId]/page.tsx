e4e7fa6205af3a76a4369487d7ae098434043613
vipulkr059 <vipulkr059@gmail.com>
2023-09-14 02:16:19 +0530

    wip : chat messages component and API

--- discord-clone/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ discord-clone/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import ChatHeader from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessage } from "@/components/chat/chat-message";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -41,7 +42,20 @@ const ChannelIdPage = async ({ params }: ChannelPageProps) => {
         type="channel"
         name={channel.name}
       />
-      <div className="flex-1"> sadasdas Future Message</div>
+      <ChatMessage
+        member={member}
+        name={channel.name}
+        chatId={channel.id}
+        type="channel"
+        apiUrl="/api/messages"
+        socketUrl="/api/socket/messages"
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        paramKey="channelId"
+        paramValue={channel.serverId}
+      />
       <ChatInput
         name={channel.name}
         type="channel"
