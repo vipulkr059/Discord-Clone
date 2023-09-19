75df7100e8acf087f8e74caeaaaed7f829aebb72
vipulkr059 <vipulkr059@gmail.com>
2023-09-20 00:01:54 +0530

    fix :fixed buges related to next version

--- discord-clone/components/chat/chat-header.tsx
+++ discord-clone/components/chat/chat-header.tsx
@@ -18,9 +18,8 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
       {type === "conversation" && (
-        <UserAvatar src={imageUrl} className="h-8 w-8 md:w-8 mr-2" />
+        <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
       )}
-
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
       <div className="ml-auto flex items-center">
         {type === "conversation" && <ChatVideoButton />}
